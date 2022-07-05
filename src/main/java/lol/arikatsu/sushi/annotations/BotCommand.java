c8201048fbbb85cac95169edd5e120b3638218e8
KingRainbow44 <kobedo11@gmail.com>
2022-07-05 14:00:00 +0100

    Add annotations

---
+++ src/main/java/lol/arikatsu/sushi/annotations/BotCommand.java
@@ -0,0 +1,11 @@
+package lol.arikatsu.sushi.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * A command that should be registered on start-up.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BotCommand {
+}
